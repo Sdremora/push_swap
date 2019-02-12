b4fa6c2dab5d91b314689df2d3cc7078f77ed6c4
sdremora <sdremora@sinys.ru>
2019-02-12 11:50:57 +0300

    init commit

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -1,19 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_push_swap.c                                   :+:      :+:    :+:   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sdremora <sdremora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/07 17:43:04 by sdremora          #+#    #+#             */
-/*   Updated: 2019/02/07 18:42:54 by sdremora         ###   ########.fr       */
+/*   Created: 2019/02/12 09:53:36 by sdremora          #+#    #+#             */
+/*   Updated: 2019/02/12 10:43:40 by sdremora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
-int	main(void)
+# define SORT_COUNT 2
+
+# include "libft.h"
+# include "ps_stack.h"
+
+typedef		struct s_solution
 {
-	ft_printf("push_swap here =)\n");
-	return (0);
-}
+	int		count;
+	char	*log_str;
+}			t_solution;
+
+void		ps_logic(t_stack *stack_a);
+
+#endif
---
+++ srcs/push_swap/ps_logic.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ps_logic.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdremora <sdremora@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/12 10:42:24 by sdremora          #+#    #+#             */
+/*   Updated: 2019/02/12 11:31:39 by sdremora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_solution	first_sort(t_stack stack_a)
+{
+	t_solution	solution;
+
+	solution.count = 2;
+	solution.log_str = "sa\npb";
+	stack_a.prev = NULL;
+	return (solution);
+}
+
+t_solution	second_sort(t_stack stack_a)
+{
+	t_solution	solution;
+
+	solution.count = 4;
+	solution.log_str = "sa\npb\npb\npb";
+	stack_a.prev = NULL;
+	return (solution);
+}
+
+int		get_min_solution(t_solution *solution)
+{
+	int min;
+	int min_count;
+	int cur_count;
+	int i;
+
+	min = 0;
+	min_count = (solution[min]).count;
+	i = 1;
+	while (i < SORT_COUNT)
+	{
+		cur_count = (solution[i]).count;
+		if (min_count > cur_count)
+		{
+			min = i;
+			min_count = cur_count;
+		}
+		i++;
+	}
+	return (min);
+}
+
+void	print_solution(t_solution *solution)
+{
+	ft_printf("%s", solution->log_str);
+}
+
+void	ps_logic(t_stack *stack_a)
+{
+	t_solution	solution[SORT_COUNT];
+	int min;
+
+	solution[0] = first_sort(*stack_a);
+	solution[1] = second_sort(*stack_a);
+	min = get_min_solution(solution);
+	print_solution(&(solution[min]));
+}
