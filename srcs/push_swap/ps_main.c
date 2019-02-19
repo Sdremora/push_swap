c3f4aca46c437fd960940648f0e9f3d7f48e79d5
sdremora <sdremora@sinys.ru>
2019-02-19 16:55:25 +0300

    fixed \n in 1 args

--- srcs/push_swap/ps_main.c
+++ srcs/push_swap/ps_main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   ps_main.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sdremora <sdremora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/07 17:43:04 by sdremora          #+#    #+#             */
-/*   Updated: 2019/02/18 16:27:28 by sdremora         ###   ########.fr       */
+/*   Updated: 2019/02/19 16:52:02 by sdremora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,8 @@ static int	error_print(void)
 
 static void	print_resolve(t_resolve *resolve)
 {
-	ft_printf("%s\n", resolve->log_str);
+	if (resolve->log_len != 0)
+		ft_printf("%s\n", resolve->log_str);
 }
 
 int			main(int argc, char **argv)
