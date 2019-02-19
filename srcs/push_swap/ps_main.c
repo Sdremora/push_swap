7a9b14dc68138b232691433b159b890e4f514971
sdremora <sdremora@sinys.ru>
2019-02-19 16:59:32 +0300

    fix back

--- srcs/push_swap/ps_main.c
+++ srcs/push_swap/ps_main.c
@@ -6,7 +6,7 @@
 /*   By: sdremora <sdremora@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/07 17:43:04 by sdremora          #+#    #+#             */
-/*   Updated: 2019/02/19 16:52:02 by sdremora         ###   ########.fr       */
+/*   Updated: 2019/02/19 16:58:57 by sdremora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,7 @@ static int	error_print(void)
 
 static void	print_resolve(t_resolve *resolve)
 {
-	if (resolve->log_len != 0)
-		ft_printf("%s\n", resolve->log_str);
+	ft_printf("%s\n", resolve->log_str);
 }
 
 int			main(int argc, char **argv)
